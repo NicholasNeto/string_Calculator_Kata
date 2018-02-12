0af0e7ddbdcd0eb1800986c8c4d468594455fc56
NicholasNeto <nicholasneto@outlook.com>
2018-02-12 11:49:52 -0200

    refatoramento de logica e test antecipando o split

--- test/step1.js
+++ test/step1.js
@@ -4,18 +4,24 @@ var expect = require('chai').expect;
 
 describe('Calculadora Kata', () => {
   describe('add', () => {
-    describe('step1', () => {
-      it('Quando enviado string vazia retorne 0', () => {
+      it('Step1: Quando enviado string vazia retorne 0', () => {
         expect(add('')).to.equal(0)
       });
 
-      it('Quando enviado apenas um numero retorne o proprio numero', () => {
+      it('STep1: Quando enviado apenas um numero retorne o proprio numero', () => {
         expect(add('1')).to.equal(1)
       });
 
-      it('Quando enviado dois numeros retorne a soma dos dois', () => {
+      it('Step1: Quando enviado apenas um numero retorne o proprio numero', () => {
+        expect(add('123')).to.equal(123)
+      });
+
+      it('Step1: Quando enviado dois numeros retorne a soma dos dois', () => {
         expect(add('1,2')).to.equal(3)
       });
-    })
+
+      it('Stpe1: Quando enviado dois numeros retorne a soma dos dois', () => {
+        expect(add('123,456')).to.equal(579)
+      });
   });
 });
