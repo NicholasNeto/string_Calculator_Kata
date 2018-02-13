f9b60852c41645544048acb33cf0408cc1424e70
NicholasNeto <nicholasneto@outlook.com>
2018-02-13 20:34:38 -0200

    removendo testes desnecessarios

--- test/step1.js
+++ test/step1.js
@@ -4,24 +4,20 @@ var expect = require('chai').expect;
 
 describe('Calculadora Kata', () => {
   describe('add', () => {
-      it('Step1: Quando enviado string vazia retorne 0', () => {
-        expect(add('')).to.equal(0)
-      });
+    it('Quando enviado string vazia retorne 0', () => {
+      expect(add('')).to.equal(0)
+    });
 
-      it('STep1: Quando enviado apenas um numero retorne o proprio numero', () => {
-        expect(add('1')).to.equal(1)
-      });
+    it('Quando enviado apenas um numero retorne o proprio numero', () => {
+      expect(add('1')).to.equal(1)
+      expect(add('123')).to.equal(123)
+    });
 
-      it('Step1: Quando enviado apenas um numero retorne o proprio numero', () => {
-        expect(add('123')).to.equal(123)
-      });
+    it('Quando enviado dois numeros retorne a soma dos dois', () => {
+      expect(add('1,2')).to.equal(3)
+      expect(add('33,45')).to.equal(78)
+      expect(add('123,456')).to.equal(579)
+    });
 
-      it('Step1: Quando enviado dois numeros retorne a soma dos dois', () => {
-        expect(add('1,2')).to.equal(3)
-      });
-
-      it('Stpe1: Quando enviado dois numeros retorne a soma dos dois', () => {
-        expect(add('123,456')).to.equal(579)
-      });
   });
 });
--- test/step3.js
+++ test/step3.js
@@ -4,44 +4,29 @@ var expect = require('chai').expect;
 describe('Calculadora Kata', () => {
   describe('add', () => {
 
-      it('Step1: Quando enviado string vazia retorne 0', () => {
+      it('Quando enviado string vazia retorne 0', () => {
         expect(add('')).to.equal(0)
       });
 
-      it('STep1: Quando enviado apenas um numero retorne o proprio numero', () => {
+      it('Quando enviado apenas um numero retorne o proprio numero', () => {
         expect(add('1')).to.equal(1)
-      });
-
-      it('Step1: Quando enviado apenas um numero retorne o proprio numero', () => {
         expect(add('123')).to.equal(123)
       });
 
-      it('Step1: Quando enviado dois numeros retorne a soma dos dois', () => {
+      it('Quando enviado dois numeros retorne a soma dos dois', () => {
         expect(add('1,2')).to.equal(3)
-      });
-
-      it('Stpe1: Quando enviado dois numeros retorne a soma dos dois', () => {
+        expect(add('33,45')).to.equal(78)
         expect(add('123,456')).to.equal(579)
       });
 
-      it('step2: Permitir que add receba quantidade desconhecida de números (passando um numero)', () => {
-        expect(add('1')).to.equal(1)
-      });
-
-      it('step2: Permitir que add receba quantidade desconhecida de números (passando dois numeros)', () => {
-        expect(add('33,45')).to.equal(78)
-      });
-
-      it('step2: Permitir que add receba quantidade desconhecida de números (passando mais de dois numeros)', () => {
+      it('Permitir que add receba quantidade desconhecida de números (passando mais de dois numeros)', () => {
         expect(add('1,2,3,4,5,6')).to.equal(21)
       });
 
-      it('step3: Deve aceitar diferentes tipos de entrada', () => {
+      it('Deve aceitar diferentes tipos de entrada', () => {
         expect(add("1\n2,3")).to.equal(6)
-      });
-
-      it('step3: Deve aceitar diferentes tipos de entrada', () => {
         expect(add("22\n33,43")).to.equal(98)
       });
+
   });
 });
