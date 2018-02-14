e64e63e078f8a4429bf0930a587059b4a2aed113
NicholasNeto <nicholasneto@outlook.com>
2018-02-14 06:12:44 -0200

    enviando refatoramento do step3  ainda com problemas na logica tentando converterr um a , em inteiro retornando NaN

--- step3.js
+++ step3.js
@@ -18,16 +18,37 @@ var add = function (dados) {
   return resultado
 }
 
+//expect(add("1\n2,3")).to.equal(6)
+//expect(add("22\n33,43")).to.equal(98)
+//expect(add("22,33,44,33,6,\n43")).to.equal(181)
+//expect(add("22,33,44,\n33,\n43")).to.equal(175)
+
+
+
+//    1\n2,3
 var criaLista = function(dados){
   var lista = []
 
+
   var preLista = dados.split('\n');
-  lista.push(parseInt(preLista[0]));
+  console.log('>>>>>>>>> preLISTA >>>>>>>>>>>>',preLista)
 
-  var numeros = preLista[1].split(',')
+  for(var x = 0; x < preLista.length; x++) {
 
-  for(var x = 0; x < numeros.length; x++){
-    lista.push(parseInt(numeros[x]))
+
+    if(preLista[x].indexOf(',') != -1) {
+      console.log('estou dentro do verdadeiro')
+      var listaSecundaria = preLista[x].split(',')
+
+      for(var y = 0; y < listaSecundaria.length; y++){
+        lista.push(listaSecundaria[y])
+      }
+
+    } else {
+      console.log('estou dentro do false  ')
+      lista.push(preLista[x])
+
+    }
   }
   return lista
 }
