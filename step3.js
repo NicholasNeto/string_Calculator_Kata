2c9e2f507401345131c1d673f6cc7f417942a36e
NicholasNeto <nicholasneto@outlook.com>
2018-02-15 10:18:50 -0200

    refatorando function criarLista  AND deixando um retorno de if mais explicito

--- step3.js
+++ step3.js
@@ -4,45 +4,22 @@ var add = function (dados) {
 
   if(stringVazia(dados)){
     return 0
-  }
-
-  if (dados.indexOf('\n') !== -1 ) {
+  } else if (dados.indexOf('\n') !== -1 ) {
     var listaNumeros = criaLista(dados)
   } else {
     var listaNumeros = dados.split(',')
   }
 
-  var resultado = listaNumeros.reduce(function (numero, cadaletra) {
-      if(!isNaN(cadaletra)){
-        return numero + parseInt(cadaletra);
-      }
-
-    }, 0);
+  var resultado = listaNumeros.reduce(function (numero, cadaLetra) {
+    return numero + parseInt(cadaLetra)
+  }, 0)
 
   return resultado
 }
 
 var criaLista = function (dados) {
-  var lista = []
-  var preLista = dados.split('\n')
-
-  for (var x = 0; x < preLista.length; x++) {
-    if (preLista[x].indexOf(',') !== -1) {
-      var listaSecundaria = preLista[x].split(',')
-
-      for (var y = 0; y < listaSecundaria.length; y++) {
-        if (stringVazia(listaSecundaria[y])) {
-          lista.push(0)
-        } else {
-          if (isNaN(listaSecundaria[y]) === false) {
-            lista.push(listaSecundaria[y])
-          }
-        }
-      }
-    } else {
-      lista.push(preLista[x])
-    }
-  }
+  var preLista = dados.replace(/\n/g, ',').replace(/,,/g, ',0,')
+  var lista = preLista.split(',')
   return lista
 }
 
