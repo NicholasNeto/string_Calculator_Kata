9c0d83ebaa6c72dbd443b0966f29ce895b1cb63b
NicholasNeto <nicholasneto@outlook.com>
2018-02-16 10:47:08 -0200

    removendo function criarLista  e refatorando cdg

--- step3.js
+++ step3.js
@@ -4,23 +4,16 @@ var add = function (dados) {
 
   if(stringVazia(dados)){
     return 0
-  } else if (dados.indexOf('\n') !== -1 ) {
-    var listaNumeros = criaLista(dados)
-  } else {
-    var listaNumeros = dados.split(',')
   }
+
+  var preLista = dados.replace(/\n/g, ',')
+  var listaNumeros = preLista.split(',')
 
   var resultado = listaNumeros.reduce(function (numero, cadaLetra) {
     return numero + parseInt(cadaLetra)
   }, 0)
 
   return resultado
-}
-
-var criaLista = function (dados) {
-  var preLista = dados.replace(/\n/g, ',')
-  var lista = preLista.split(',')
-  return lista
 }
 
 var stringVazia = function (entrada) {
